137837d69a012e928771e1cd10ac639d20037340
andrewvoyts01@gmail.com <3336666r>
2021-11-05 12:07:32 +0200

    homework is done

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width,user-scalable = no, initial scale = 1.0, maximum scale=1.0, minimum scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Document</title>
+</head>
+<body>
+<form action=""  method="post">
+    <br><span>Name</span></br>
+    <br> <input type="text" name="name"></br>
+    <br> <span>Password</span></br>
+    <br> <input type="password" name="password"></br>
+    <br> <span>Email</span></br>
+    <br><input type="email" name="email"></br>
+    <span>Birth Date</span>
+    <br><input type="date" name="birth_date"></br>
+    <br><span>Gender:</span></br>
+    <br><span>Male</span></br>
+    <input type="radio" name="gender" id="2" value="male">
+    <br><span>Female</span></br>
+    <input type="radio" name="gender" id="1" value="female"></br>
+    <span>Favourite colour</span>
+    <br><input type="text" name="colour"></br>
+    <input type="submit">
+</form>
+
+<table border="1">
+    <tr>
+        <th><?php echo "Name"?></th>
+        <th><?php echo $_POST['name'] ?></th>
+    </tr>
+    <tr>
+        <th><?php echo "Password"?></th>
+        <th><?php echo $_POST['password']?></th>
+    </tr>
+    <tr>
+        <th><?php echo "Email"?></th>
+        <th><?php echo $_POST['email']?></th>
+    </tr>
+    <tr>
+        <th><?php echo "Birth Date"?></th>
+        <th><?php echo $_POST['birth_date']?></th>
+    </tr>
+    <tr>
+        <th><?php echo "Gender"?></th>
+        <th><?php echo $_POST['gender']?></th>
+    </tr>
+    <tr>
+        <th><?php echo "Favourite colour"?></th>
+        <th><?php echo $_POST['colour']?></th>
+    </tr>
+</table>
+</body>
+</html>
